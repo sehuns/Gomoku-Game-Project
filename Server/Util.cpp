917de820086b576f1f19957d496578e87e40864e
sehuns <sehun9707@naver.com>
2021-06-21 13:52:10 +0900

    Source Code Refactoring

---
+++ Server/Util.cpp
@@ -0,0 +1,10 @@
+#include "Util.h"
+vector<string> Util::getTokens(string input, char delimiter) {
+	vector<string> tokens;
+	istringstream f(input);
+	string s;
+	while (getline(f, s, delimiter)) {
+		tokens.push_back(s);
+	}
+	return tokens;
+}
